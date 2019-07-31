b7ceaf781c8eb708ca277705d1705a9da81cdacc
James Curran <james.curran@gmail.com>
2019-07-31 19:39:42 -0400

    ANi Gif for Comb & Quick

--- CombSort/Program.cs
+++ CombSort/Program.cs
@@ -1,5 +1,7 @@
 ﻿using SortExtravaganza.Common;
 using System;
+using System.Drawing;
+using Common;
 
 namespace CombSort
 {
@@ -23,47 +25,61 @@ class CombSort
 
     static void Sort(int[] array)
     {
-        int length = array.Length;
+	    int length = array.Length;
+	    int[] hilite = new Int32[2];
 
-        int gap = length;
+	    int gap = length;
 
-        //We initialize this as true to enter the while loop.
-        bool swapped = true;
+	    //We initialize this as true to enter the while loop.
+	    bool swapped = true;
 
-        while (gap != 1 || swapped == true)
-        {
-            gap = GetNextGap(gap);
+	    while (gap != 1 || swapped == true)
+	    {
+		    gap = GetNextGap(gap);
 
-            //Set swapped as false.  Will go to true when two values are swapped.
-            swapped = false;
+		    //Set swapped as false.  Will go to true when two values are swapped.
+		    swapped = false;
 
-            //Compare all elements with current gap 
-            for (int i = 0; i < length - gap; i++)
-            {
-                if (array[i] > array[i + gap])
-                {
-                    //Swap
-                    int temp = array[i];
-                    array[i] = array[i + gap];
-                    array[i + gap] = temp;
+		    //Compare all elements with current gap 
+		    for (int i = 0; i < length - gap; i++)
+		    {
 
-                    swapped = true;
-                }
-            }
-        }
+			    hilite[0] = i;
+			    hilite[1] = i + gap;
+			    graph = CommonFunctions.GraphArray(array, hilite, Color.Chocolate);
+			    gifWriter.WriteFrame(graph);
+
+			    if (array[i] > array[i + gap])
+			    {
+				    //Swap
+				    int temp = array[i];
+				    array[i] = array[i + gap];
+				    array[i + gap] = temp;
+
+				    swapped = true;
+				    graph = CommonFunctions.GraphArray(array, hilite, Color.Yellow);
+				    gifWriter.WriteFrame(graph);
+			    }
+		    }
+	    }
     }
 
-    public static void Main()
-    {
-        int[] array = { 10, 28, 1, 55, 6, 21, 36, 3, 45, 15, 0 };
+    private static IGifWriter gifWriter;
+    private static Image graph;
 
-        Console.WriteLine("Comb Sort");
+		public static void Main()
+    {
+        //int[] array = { 10, 28, 1, 55, 6, 21, 36, 3, 45, 15, 0 };
+        int[] array = CommonFunctions.CreateTestArray(33, TestType.Shuffled);
+
+			Console.WriteLine("Comb Sort");
 
         CommonFunctions.PrintInitial(array);
 
-        Sort(array);
+        using (gifWriter = new GifWriter(@"C:\Temp\CombSort.33.ani.gif", 150, 0))
+	        Sort(array);
 
-        CommonFunctions.PrintFinal(array);
+			CommonFunctions.PrintFinal(array);
         Console.ReadLine();
 
     }
--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -1,5 +1,8 @@
 ﻿using SortExtravaganza.Common;
 using System;
+using System.Drawing;
+using System.Linq;
+using Common;
 
 namespace QuickSort
 {
@@ -18,14 +21,20 @@ class QuickSort
     static int Partition(int[] array, int low,
                                     int high)
     {
-        //1. Select a pivot point.
-        int pivot = array[high];
+	    int[] hilite = Enumerable.Range(low, high - low + 1).ToArray();
+	    graph = CommonFunctions.GraphArray(array, hilite, Color.BlanchedAlmond);
+	    gifWriter.WriteFrame(graph);
+
+			//1. Select a pivot point.
+			int pivot = array[high];
 
         int lowIndex = (low - 1);
 
         //2. Reorder the collection.
         for (int j = low; j < high; j++)
         {
+	        graph = CommonFunctions.GraphArray(array, hilite, Color.BlanchedAlmond);
+	        gifWriter.WriteFrame(graph);
             if (array[j] <= pivot)
             {
                 lowIndex++;
@@ -55,14 +64,22 @@ class QuickSort
         }
     }
 
-    public static void Main()
+    private static IGifWriter gifWriter;
+    private static Image graph;
+
+
+		public static void Main()
     {
         int[] array = { 72, 12, 6, 33, 81, 97, 37, 59, 52, 1, 20 };
-        int length = array.Length;
+//        int[] array = CommonFunctions.CreateTestArray(33, TestType.Shuffled);
+
+			int length = array.Length;
 
         Console.WriteLine("QuickSort");
         CommonFunctions.PrintInitial(array);
-        Sort(array, 0, length - 1);
+
+        using (gifWriter = new GifWriter(@"C:\Temp\QuickSort.11.ani.gif", 150, 0))
+			Sort(array, 0, length - 1);
 
         CommonFunctions.PrintFinal(array);
 
